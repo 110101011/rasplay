854ec11362f16cf45bceef5b884c0e96537ed981
Justin Verel <justin@marverinc.nl>
2018-05-27 18:11:36 +0000

    Little fix-up

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 
-""Display Mode (I2C or GPIO)""
+"""Display Mode (I2C or GPIO)"""
 lcd_mode = "I2C"
 
-""I2C""
+"""I2C"""
 # PCF8574, MCP23005 of MCP23008
 expander = "PCF8574"
 address = 0x27
 port = 1
 backlight_enabled = "True"
 
-""GPIO""
+"""GPIO"""
 pin_rs = None
 pin_rw = None
 pin_e = None
 pins_data = None
 numbering_mode = None
 
-""Shared""
+"""Shared"""
 cols = 20
 rows = 4
 dotsize = 8
