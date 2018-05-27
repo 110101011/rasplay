1b0911693320685e56006af181dbfdf5137cd1af
Justin Verel <justin@marverinc.nl>
2018-05-27 18:05:44 +0000

    Added background task to poll for remote keypresses, and cleaned up the various informational screens

--- rasplay.py
+++ rasplay.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.5
 
 """rasplay.py: Python script that display's system information on to a 
 HD44870 charlcd from a Raspberry Pi either via GPIO or I2C"""
@@ -10,7 +10,7 @@ import os.path
 import logging, sys
 import socket
 import subprocess
-import asyncio
+import threading
 
 import config
 
@@ -30,6 +30,7 @@ first_run = True
 SOCKPATH = "/var/run/lirc/lircd"
 
 sock = None
+live = True
 
 graden = (
 	0b00000,
@@ -46,39 +47,61 @@ logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 def main():
 	#main function
+	global now_key
+
 	logging.debug("Starting up display")
 	setup_display()
 	logging.debug("Starting up irw socket")
 	init_irw()
 
+	now_key = "KEY_1"
+	key = "KEY_1"
+
+	thread = NextKey(next_key)
+	thread.start()
+
 	lcd.write_string('Raspberry Pi')
 	lcd.cursor_pos = (2, 0)
 	lcd.write_string('Rasplay Booting up')
 	
-	live = True
-
 	time.sleep(2)	
-
-	now_key = 'KEY_1'
+	lcd.clear()
 
 	#Create While loop
 	logging.debug("Starting while loop")
 	while live == True:
-		now_key = next_key()
-		logging.debug("now_key == " + now_key)
-		while now_key == 'KEY_1':
-			logging.debug("show_date()")
+		#logging.debug("now_key = %s", now_key)
+
+		if key != now_key:
+			lcd.clear()
+
+		key = now_key
+		if now_key == 'KEY_1':
+			logging.debug("show_date() - " + now_key)
+			key = now_key
 			show_date()
 			time.sleep(1)
-						
 			for rows in range(len(lcddata)):
 				lcd.cursor_pos = (lcddata[rows][0], lcddata[rows][1])
 				lcd.write_string(lcddata[rows][2])
-			if now_key != "KEY_1":
-				break
-		while now_key == "":
+		elif now_key == "KEY_2":
+			logging.debug("show_cpu()- " + now_key)
+			key = now_key
+			show_cpu()
+			time.sleep(1)
+			for rows in range(len(lcddata)):
+				lcd.cursor_pos = (lcddata[rows][0], lcddata[rows][1])
+				lcd.write_string(lcddata[rows][2])
+		elif now_key == "KEY_3":
+			logging.debug("show_network() - " + now_key)
+			show_network()
+			time.sleep(1)
+			for rows in range(len(lcddata)):
+				lcd.cursor_pos = (lcddata[rows][0], lcddata[rows][1])
+				lcd.write_string(lcddata[rows][2])
+		else:
 			print("Fucked")
-			break
+			time.sleep(1)
 
 	#Close connection to lcd and clear
 	lcd.close(clear=True)
@@ -111,14 +134,29 @@ def init_irw():
 	logging.debug('starting up on %s' % SOCKPATH)
 	sock.connect(SOCKPATH)
 
-async def next_key():
-	while True:
-		data = sock.recv(128)
-		data.strip()
-		if data:
-			break
-	words = data.split()
-	return str(words[2])
+class NextKey(threading.Thread):
+
+	def __init__(self, function_next_key):
+		threading.Thread.__init__(self)
+		self.runnable = function_next_key
+		self.deamon = True
+
+	def run(self):
+		self.runnable()
+
+def next_key():
+	global now_key
+	while live == True:
+		while True:
+			data = sock.recv(128)
+			data.strip()
+			if data:
+				break
+		words = data.split()
+		key = str(words[2])
+		key = key.split("b'")[1]
+		key = key.split("'")[0]
+		now_key = key
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -133,53 +171,33 @@ def get_ip():
     return IP
 
 def getTemp():
-    """Get Temperature""" 
-    cpu = subprocess.Popen("cat /sys/class/thermal/thermal_zone0/temp", shell=True, stdout=subprocess.PIPE).stdout.read()
-    cpu = float(cpu) / 1000
-    cpu = round(cpu, 1)
-    gpu = subprocess.Popen("vcgencmd measure_temp", shell=True, stdout=subprocess.PIPE).stdout.read()
-    gpu = gpu.replace("temp=", "")
-    gpu = gpu.replace("'C", "\x00C")
-    return cpu, gpu
+	"""Get Temperature""" 
+	cpu = subprocess.Popen("cat /sys/class/thermal/thermal_zone0/temp", shell=True, stdout=subprocess.PIPE).stdout.read()
+	cpu = float(cpu) / 1000
+	cpu = round(cpu, 1)
+
+	gpu = subprocess.Popen("vcgencmd measure_temp", shell=True, stdout=subprocess.PIPE).stdout.read()
+	gpu = gpu.decode('utf-8')
+	gpu = gpu.replace("temp=", "")
+	gpu = gpu.replace("'C", "\x00C")
+
+	return cpu, gpu
 
 def get_cpu_speed():
     cpu_speed = subprocess.Popen("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq", shell=True, stdout=subprocess.PIPE).stdout.read()
     cpu_speed = int(cpu_speed) / 1000
     return cpu_speed
 
-def show_info():
-	#Show information on display
-	global lcddata
-#	if first_run == True:
-#		lcd.clear()
-#		first_run = False
-#		logging.debug(first_run)
-
-	ip_address = get_ip()
-
-	cpu, gpu = getTemp()
-
-	lcd.create_char(0, graden)
-
-	lcd.cursor_pos = (1, 0)
-	lcd.write_string("IP:  " + ip_address)
-	lcd.cursor_pos = (2, 0)
-	lcd.write_string("CPU: " + str(get_cpu_speed()) + "Mhz")
-	lcd.cursor_pos = (2, 14)
-	lcd.write_string(str(cpu) + "\x00C")
-	lcd.cursor_pos = (3, 0)
-	lcd.write_string("GPU:")
-	lcd.cursor_pos = (3, 14)
-	lcd.write_string(str(gpu))
-
 def show_date():
 	#Show Date and Time
 	global lcddata
 
+	day = datetime.datetime.now().strftime("%A")
+
 	date = datetime.datetime.now().strftime("%d-%m-%y")
 	time = datetime.datetime.now().strftime("%H:%M:%S")
 
-	lcddata = [0, 0, date], [0, 12, time]
+	lcddata = [0, 0, day], [0, 12, time], [1, 0, date]
 
 	return lcddata
 
@@ -191,37 +209,41 @@ def show_cpu():
 
 	lcd.create_char(0, graden)
 
-	cpu_speed_string = "CPU: " + str(get_cpu_speed()) + " Mhz"
+	cpu_string = "CPU: "
+	cpu_speed_string = str(get_cpu_speed()) + " Mhz"
 	cpu_temp_string = str(cpu) + "\x00C"
 	gpu_string = "GPU:"
 	gpu_temp_string = str(gpu)
 
-	print(cpu_speed_string)
-	print(cpu_temp_string)
-	print(gpu_string)
-	print(gpu_temp_string)
-
-	lcddata = [0, 0, cpu_speed_string], [0, 14, cpu_temp_string], [1, 0, gpu_string], [1, 14, gpu_temp_string]
+	lcddata = [0, 0, cpu_string], [0, 11, cpu_speed_string], [1, 14, cpu_temp_string], [2, 0, gpu_string], [3, 14, gpu_temp_string]
 
 	return lcddata
 
 def show_network():
 	#Show Network information
-	get_command()
+	global lcddata
 
-def get_command(key):
-	#Get Command from remote
-	logging.debug("Key: %s", key)
-	global first_run
-	if key == "KEY_0":
-		first_run = True
-		show_info()
-	elif key == "KEY_1":
-		first_run = True
-		show_date()
-	else:
-		first_run = True
-		show_info()
+	ip_address = get_ip()
+
+	ip_string = "ip address:"
+
+	lcddata = [0, 0, ip_string], [1, 0, ip_address]
+
+	return lcddata
+
+#def get_command(key):
+#	#Get Command from remote
+#	logging.debug("Key: %s", key)
+#	global first_run
+#	if key == "KEY_0":
+#		first_run = True
+#		show_info()
+#	elif key == "KEY_1":
+#		first_run = True
+#		show_date()
+#	else:
+#		first_run = True
+#		show_info()
 
 if __name__ == "__main__":
 	main()
